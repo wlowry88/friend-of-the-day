f0423638b917614b5ddf9fbd22b1e7f3dacf7ea9
cassidypignatello <cassidy.pignatello@gmail.com>
2014-07-30 17:33:34 -0400

    trying to fix login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,30 @@
 class SessionsController < ApplicationController
   def create
     @response = env["omniauth.auth"]
-    @user = User.from_omniauth(@response)
-    friends = @user.get_friends(@response)
-    @user.create_friends(friends)
-    session[:user_id] = @user.id
+    if User.where(@response.slice(:provider, :uid))
+      @user = User.from_omniauth(@response)
+      session[:user_id] = @user.id
+    else
+      @user = User.from_omniauth(@response)
+      friends = @user.get_friends(@response)
+      @user.create_friends(friends)
+      session[:user_id] = @user.id
+    end
     redirect_to root_path
 
-    # This is sessions controller for calendar
-    @auth = request.env["omniauth.auth"]
-    #Use the token from the data to request a list of calendars
-    @token = @auth["credentials"]["token"]
-    client = Google::APIClient.new
-    client.authorization.access_token = @token
-    service = client.discovered_api('calendar', 'v3')
-    @result = client.execute(
-      :api_method => service.events.list,
-    # we need to figure out how to get the birthday calendar id for all users"
-      :parameters => {'calendarId' => '8bedhb3o4g5l2dtkil2k34tjm2dgum9k@import.calendar.google.com'},
-      :headers => {'Content-Type' => 'application/json'})
-    # binding.pry
+    # # This is sessions controller for calendar
+    # @auth = request.env["omniauth.auth"]
+    # #Use the token from the data to request a list of calendars
+    # @token = @auth["credentials"]["token"]
+    # client = Google::APIClient.new
+    # client.authorization.access_token = @token
+    # service = client.discovered_api('calendar', 'v3')
+    # @result = client.execute(
+    #   :api_method => service.events.list,
+    # # we need to figure out how to get the birthday calendar id for all users"
+    #   :parameters => {'calendarId' => '8bedhb3o4g5l2dtkil2k34tjm2dgum9k@import.calendar.google.com'},
+    #   :headers => {'Content-Type' => 'application/json'})
+    
   end
 
   def destroy

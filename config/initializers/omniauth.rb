2e188f1c2598f38c60ec7a606b6b4644d8d61588
Will Lowry <william.c.lowry@gmail.com>
2014-07-31 11:02:18 -0400

    resets google omniauth callback urls for production and development

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,16 @@
 OmniAuth.config.logger = Rails.logger
 
+if ENV['RAILS_ENV'] == 'production'
+	HOST = "intouchbetter.herokuapp.com"
+else
+	HOST = "localhost:3000"
+end
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '275700772346-ibg9fas3drh0pvephdpjklsklnkrp6na.apps.googleusercontent.com', 'TzsunqLxldnOSJ7orBfEK514', {
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/userinfo.email, https://www.googleapis.com/auth/calendar, https://www.google.com/m8/feeds, https://www.googleapis.com/auth/plus.login, openid, https://www.googleapis.com/auth/plus.circles.read, https://www.googleapis.com/auth/plus.me', 
-    redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
+    redirect_uri:"http://#{HOST}/auth/google_oauth2/callback"
   }
 end

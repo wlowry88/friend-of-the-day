ae4f1cf548a661200660c8dcee9939c678c9dec8
Will Lowry <william.c.lowry@gmail.com>
2014-07-31 11:32:51 -0400

    succesful deploy on heroku - sets up heroku client key and secret

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
       @user = User.from_omniauth(@response)
       friends = @user.get_friends(@response)
       @user.create_friends(friends)
-      @user.get_plus_info(@response)
+      #@user.get_plus_info(@response)
       @user.remove_duplicates
       session[:user_id] = @user.id
       redirect_to friends_path

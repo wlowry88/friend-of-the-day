33cb3df662ea0b7f4d24674548f3567cdc1e9c8c
Will Lowry <william.c.lowry@gmail.com>
2014-07-31 21:29:46 -0400

    fun jQuery slideToggle method for edit form

--- app/assets/javascripts/friends.js
+++ app/assets/javascripts/friends.js
@@ -2,7 +2,6 @@ $(function(){
   $(".edit_friend_form").hide();
   $(".edit-friend-button").on("click", function(e){
     e.preventDefault();
-    $(this).parent().find(".edit_friend_form").first().slideToggle();
+    $(this).parent().find(".edit_friend_form").first().css("visibility", "visible").slideToggle();
   })
-
 });

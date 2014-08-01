3e1b030eb0db3715a9e885bb0d625cd154d84065
Will Lowry <william.c.lowry@gmail.com>
2014-08-01 12:27:02 -0400

    layout change

--- app/assets/javascripts/friends.js
+++ app/assets/javascripts/friends.js
@@ -1,10 +1,16 @@
 $(function(){
   $(".edit_friend_form").hide();
   $("#normal_friends_list").hide();
+  $(".edit_number_form").hide();
 
   $(".edit-friend-button").on("click", function(e){
     e.preventDefault();
     $(this).parent().find(".edit_friend_form").first().css("visibility", "visible").slideToggle();
+  });
+
+  $(".settings_button").on("click", function(e){
+  	e.preventDefault();
+  	$(".edit_number_form").css("visibility", "visible").slideToggle();
   });
 
   $("#show_friends_button").on("click", function(e){

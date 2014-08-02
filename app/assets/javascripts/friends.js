38ade1eeb97c6120405b08f40d9932e49f0fb799
Will Lowry <william.c.lowry@gmail.com>
2014-08-02 04:07:59 -0400

    fixes ui with links

--- app/assets/javascripts/friends.js
+++ app/assets/javascripts/friends.js
@@ -20,7 +20,8 @@ $(function(){
 
   $("#show_friends_button").on("click", function(e){
   	e.preventDefault;
-  	$("#normal_friends_list").css("visibility", "visible").slideToggle();
+    $("#normal_friends_list").css("visibility", "visible").toggle();
+    $("body").scrollTo(1700, {duration: 1000});
   });
 
   $(".friend_of_the_day").css("visibility", "visible").fadeIn(1500);

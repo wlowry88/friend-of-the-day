da205ae6e4445ad25afacedf2b62ba0164207366
Denine Guy <denineguy@gmail.com>
2014-08-12 00:22:55 -0400

    Took out the methods for google calendar api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
       @user = User.from_omniauth(@response)
       friends = @user.get_friends(@response)
       @user.create_friends(friends)
-      #@user.get_plus_info(@response)
       @user.remove_duplicates
       session[:user_id] = @user.id
       redirect_to friends_path
@@ -15,19 +14,6 @@ class SessionsController < ApplicationController
       redirect_to friends_path
     end
 
-    # # This is sessions controller for calendar
-    # @auth = request.env["omniauth.auth"]
-    # #Use the token from the data to request a list of calendars
-    # @token = @auth["credentials"]["token"]
-    # client = Google::APIClient.new
-    # client.authorization.access_token = @token
-    # service = client.discovered_api('calendar', 'v3')
-    # @result = client.execute(
-    #   :api_method => service.events.list,
-    # # we need to figure out how to get the birthday calendar id for all users"
-    #   :parameters => {'calendarId' => '8bedhb3o4g5l2dtkil2k34tjm2dgum9k@import.calendar.google.com'},
-    #   :headers => {'Content-Type' => 'application/json'})
-
   end
 
   def destroy
@@ -35,8 +21,4 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  ##This is how you get a calendar Id for and search for friend's birthdays
-  ##:api_method => service.calendar_list.list,
-  ##:parameters => {},
-  ## get the calendar id
 end
